82d596f2bb268bbdeb5f2bc27a8d6c8444561122
rynzxxx <kakeksalto29@gmail.com>
2025-09-15 21:45:51 +0700

    update template

--- app/Views/admin_keuangan/bku_bulanan/new.php
+++ app/Views/admin_keuangan/bku_bulanan/new.php
@@ -155,7 +155,8 @@
     </form>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/imask/7.6.1/imask.min.js" integrity="sha512-+3RJc0aLDkj0plGNnrqlTwCCyMmDCV1fSYqXw4m+OczX09Pas5A/U+V3pFwrSyoC1svzDy40Q9RU/85yb/7D2A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/imask@7.6.1/dist/imask.min.js"></script>
+
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
--- app/Views/admin_keuangan/layout/template.php
+++ app/Views/admin_keuangan/layout/template.php
@@ -38,7 +38,8 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://unpkg.com/imask"></script>
+    <script src="https://cdn.jsdelivr.net/npm/imask@7.6.1/dist/imask.min.js"></script>
+
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {
